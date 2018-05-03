7e4326f580ce1c9f2780037157c321f9b76da224
Josh Fry <joshfry575@email.arizona.edu>
2018-05-02 20:13:24 -0700

    added Robert's GameBoard changes. Not tested

--- Clue/src/clueless/Hallway.java
+++ Clue/src/clueless/Hallway.java
@@ -1,21 +1,82 @@
 package clueless;
 
-public class Hallway extends Location
-{
-	String name;
+public class Hallway extends Location{
 
-	public Hallway()
-	{
-		name = "null";
-	}
+    boolean occupied = false;
+    String hallName;
 
-	public void setName(String string)
-	{
-		name = string;
-	}
+    public Hallway(int number){
+        determineHall(number);
+        setName("Hallway: " + hallName);
+    }
 
-	public String getName()
-	{
-		return name;
-	}
+    public void changeOccupied(){
+        occupied = !occupied;
+    }
+
+    private String determineHall(int number) {
+        if(number == 0) {
+            hallName = "S-to-H";
+            setLeft();
+            setRight();
+        }
+        else if(number == 1) {
+            hallName = "H-to-Lo";
+            setLeft();
+            setRight();
+        }
+        else if(number == 2) {
+            hallName = "S-to-Li";
+            setUp();
+            setDown();
+        }
+        else if(number == 3) {
+            hallName = "H-to-BiR";
+            setUp();
+            setDown();
+        }
+        else if(number == 4) {
+            hallName = "L-to-DR";
+            setUp();
+            setDown();
+        }
+        else if(number == 5) {
+            hallName = "L-to-BiR";
+            setLeft();
+            setRight();
+        }
+        else if(number == 6) {
+            hallName = "BiR-to-DR";
+            setLeft();
+            setRight();
+        }
+        else if(number == 7) {
+            hallName = "Li-to-C";
+            setUp();
+            setDown();
+        }
+        else if(number == 8) {
+            hallName = "BiR-to-BaR";
+            setUp();
+            setDown();
+        }
+        else if(number == 9) {
+            hallName = "D-to-K";
+            setUp();
+            setDown();
+        }
+        else if(number == 10) {
+            hallName = "C-to-BaR";
+            setLeft();
+            setRight();
+        }
+        else if(number == 11) {
+            hallName = "BaR-to-K";
+            setLeft();
+            setRight();
+        }        
+
+        return hallName;
+    }
+
 }
